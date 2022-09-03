b7337a15e800b9e49516c79e191b6db1ff1b3571
Jiwon Jeong <jiwonjeong414@gmail.com>
2022-09-03 12:20:47 -0700

    Touches & Calendar Marking

--- screens/MoodScreen.js
+++ screens/MoodScreen.js
@@ -1,17 +1,31 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, StyleSheet, ScrollView, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { RootContext } from "../config/RootContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { moderateScale } from "react-native-size-matters";
 import LottieView from "lottie-react-native";
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 
 const MoodScreen = () => {
   const { textTheme, setTextTheme } = React.useContext(RootContext);
   const [speed, setSpeed] = useState(1);
+  const [moodNumber, setMoodNumber] = useState(3);
 
   useEffect(() => {
     setSpeed(0.999);
   }, []);
+
+  const handleTouch = () => {
+    if (moodNumber === 5) setMoodNumber(1);
+    else setMoodNumber(moodNumber + 1);
+  };
 
   return (
     <ScrollView style={styles.container}>
@@ -20,51 +34,128 @@ const MoodScreen = () => {
           top: moderateScale(40),
           flexDirection: "row",
           flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <LottieView
-          autoPlay
-          loop
-          speed={speed}
-          style={{ width: 100, height: 100 }}
-          source={require("../assets/lottie/alien-amazing.json")}
+        <Calendar
+          style={styles.Calendar}
+          dayComponent={({ date, state }) => {
+            return (
+              <View>
+                <Text
+                  style={{
+                    textAlign: "center",
+                    color: state === "disabled" ? "gray" : "black",
+                  }}
+                >
+                  {date.day}
+                </Text>
+                <LottieView
+                  style={{ width: 100, height: 100 }}
+                  source={require("../assets/lottie/alien-amazing.json")}
+                />
+              </View>
+            );
+          }}
         />
-        <LottieView
-          autoPlay
-          loop
-          speed={speed}
-          style={{ width: 100, height: 100 }}
-          source={require("../assets/lottie/alien-happy.json")}
-        />
-        <LottieView
-          autoPlay
-          loop
-          speed={speed}
-          style={{ width: 100, height: 100 }}
-          source={require("../assets/lottie/alien-okay.json")}
-        />
-        <LottieView
-          autoPlay
-          loop
-          speed={speed}
-          style={{ width: 100, height: 100 }}
-          source={require("../assets/lottie/alien-sad.json")}
-        />
-        <LottieView
-          autoPlay
-          loop
-          speed={speed}
-          style={{ width: 100, height: 100 }}
-          source={require("../assets/lottie/alien-mad.json")}
-        />
+        {moodNumber === 1 ? (
+          <TouchableWithoutFeedback onPress={handleTouch}>
+            <LottieView
+              autoPlay
+              loop
+              speed={speed}
+              style={{
+                width: 300,
+                height: 300,
+                marginBottom: moderateScale(100),
+              }}
+              source={require("../assets/lottie/alien-mad.json")}
+            />
+          </TouchableWithoutFeedback>
+        ) : moodNumber === 2 ? (
+          <TouchableWithoutFeedback onPress={handleTouch}>
+            <LottieView
+              autoPlay
+              loop
+              speed={speed}
+              style={{
+                width: 300,
+                height: 300,
+                marginBottom: moderateScale(100),
+              }}
+              source={require("../assets/lottie/alien-sad.json")}
+            />
+          </TouchableWithoutFeedback>
+        ) : moodNumber === 3 ? (
+          <TouchableWithoutFeedback onPress={handleTouch}>
+            <LottieView
+              autoPlay
+              loop
+              speed={speed}
+              style={{
+                width: 300,
+                height: 300,
+                marginBottom: moderateScale(100),
+              }}
+              source={require("../assets/lottie/alien-okay.json")}
+            />
+          </TouchableWithoutFeedback>
+        ) : moodNumber === 4 ? (
+          <TouchableWithoutFeedback onPress={handleTouch}>
+            <LottieView
+              autoPlay
+              loop
+              speed={speed}
+              style={{
+                width: 300,
+                height: 300,
+                marginBottom: moderateScale(100),
+              }}
+              source={require("../assets/lottie/alien-happy.json")}
+            />
+          </TouchableWithoutFeedback>
+        ) : (
+          <TouchableWithoutFeedback onPress={handleTouch}>
+            <LottieView
+              autoPlay
+              loop
+              speed={speed}
+              style={{
+                width: 300,
+                height: 300,
+                marginBottom: moderateScale(100),
+              }}
+              source={require("../assets/lottie/alien-amazing.json")}
+            />
+          </TouchableWithoutFeedback>
+        )}
       </View>
     </ScrollView>
   );
 };
 
+{
+  /* <Calendar
+  style={[styles.calendar, {height: 300}]}
+  dayComponent={({date, state}) => {
+    return (
+      <View>
+        <Text style={{textAlign: 'center', color: state === 'disabled' ? 'gray' : 'black'}}>
+          {date.day}
+        </Text>
+      </View>
+    );
+  }}
+/> */
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  Calendar: {
+    width: moderateScale(350),
   },
 });
 
