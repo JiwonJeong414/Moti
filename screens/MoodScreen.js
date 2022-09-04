27b849a03e34ae4002d763ab4137f1de3d013315
Jiwon Jeong <jiwonjeong414@gmail.com>
2022-09-03 23:45:12 -0700

    Marking Test Working

--- screens/MoodScreen.js
+++ screens/MoodScreen.js
@@ -6,17 +6,20 @@ import {
   Text,
   SafeAreaView,
   TouchableWithoutFeedback,
+  Image,
 } from "react-native";
 import { RootContext } from "../config/RootContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { moderateScale } from "react-native-size-matters";
 import LottieView from "lottie-react-native";
+import { AntDesign, Feather } from "@expo/vector-icons";
 import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 
 const MoodScreen = () => {
   const { textTheme, setTextTheme } = React.useContext(RootContext);
   const [speed, setSpeed] = useState(1);
   const [moodNumber, setMoodNumber] = useState(3);
+  const [editTrue, setEditTrue] = useState(false);
 
   useEffect(() => {
     setSpeed(0.999);
@@ -31,16 +34,16 @@ const MoodScreen = () => {
     <ScrollView style={styles.container}>
       <View
         style={{
-          top: moderateScale(40),
-          flexDirection: "row",
-          flexWrap: "wrap",
+          top: moderateScale(50),
+          flex: 1,
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
         <Calendar
           style={styles.Calendar}
-          dayComponent={({ date, state }) => {
+          dayComponent={({ date, state, marking }) => {
             return (
               <View>
                 <Text
@@ -51,14 +54,41 @@ const MoodScreen = () => {
                 >
                   {date.day}
                 </Text>
-                <LottieView
-                  style={{ width: 100, height: 100 }}
-                  source={require("../assets/lottie/alien-amazing.json")}
-                />
+                {marking === undefined
+                  ? console.log(marking)
+                  : console.log(marking.type)}
+                {marking !== undefined ? (
+                  <Image
+                    style={{ width: 50, height: 100 }}
+                    source={require("../assets/image/alien-amazing.png")}
+                  />
+                ) : (
+                  <View
+                    style={{ width: 50, height: 80, backgroundColor: "gray" }}
+                  ></View>
+                )}
               </View>
             );
           }}
+          markedDates={{
+            "2022-09-03": { type: "happy" },
+            "2022-09-17": { marked: true },
+            "2022-09-30": { marked: true },
+          }}
         />
+        {editTrue === false ? (
+          <TouchableWithoutFeedback onPress={() => setEditTrue(true)}>
+            <AntDesign
+              name="pluscircleo"
+              size={moderateScale(24.2)}
+              color="black"
+            />
+          </TouchableWithoutFeedback>
+        ) : (
+          <TouchableWithoutFeedback onPress={() => setEditTrue(false)}>
+            <Feather name="edit-2" size={moderateScale(24.2)} color="black" />
+          </TouchableWithoutFeedback>
+        )}
         {moodNumber === 1 ? (
           <TouchableWithoutFeedback onPress={handleTouch}>
             <LottieView
