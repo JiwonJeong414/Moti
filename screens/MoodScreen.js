6d423d6fec570895e6789365f3a7f5a37f436412
Jiwon Jeong <jiwonjeong414@gmail.com>
2022-09-04 21:47:50 -0700

    Calendar Mood V1

--- screens/MoodScreen.js
+++ screens/MoodScreen.js
@@ -14,20 +14,47 @@ import { moderateScale } from "react-native-size-matters";
 import LottieView from "lottie-react-native";
 import { AntDesign, Feather } from "@expo/vector-icons";
 import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import moment from "moment";
 
 const MoodScreen = () => {
-  const { textTheme, setTextTheme } = React.useContext(RootContext);
+  const { textTheme, colorTheme } = React.useContext(RootContext);
   const [speed, setSpeed] = useState(1);
   const [moodNumber, setMoodNumber] = useState(3);
   const [editTrue, setEditTrue] = useState(false);
+  const [data, setData] = useState({});
 
   useEffect(() => {
     setSpeed(0.999);
   }, []);
 
+  useEffect(() => {
+    const retrieveMoodList = async () => {
+      let retrieveData = await AsyncStorage.getItem("Moods");
+      retrieveData = JSON.parse(retrieveData);
+      if (retrieveData == null) setData({});
+      else setData(retrieveData);
+    };
+    retrieveMoodList();
+  }, []);
+
   const handleTouch = () => {
     if (moodNumber === 5) setMoodNumber(1);
     else setMoodNumber(moodNumber + 1);
+  };
+
+  const handleSubmit = async () => {
+    setEditTrue(!editTrue);
+    let today = moment().format("YYYY-MM-DD");
+    // today = moment().add(-4, "d").format("YYYY-MM-DD");
+    let type = "";
+    if (moodNumber === 1) type = "mad";
+    else if (moodNumber === 2) type = "sad";
+    else if (moodNumber === 3) type = "okay";
+    else if (moodNumber === 4) type = "happy";
+    else if (moodNumber === 5) type = "amazing";
+    const newData = { ...data, [today]: { type: type } };
+    await AsyncStorage.setItem("Moods", JSON.stringify(newData));
+    setData(newData);
   };
 
   return (
@@ -43,6 +70,7 @@ const MoodScreen = () => {
       >
         <Calendar
           style={styles.Calendar}
+          theme={{ calendarBackground: colorTheme.primary }}
           dayComponent={({ date, state, marking }) => {
             return (
               <View>
@@ -54,30 +82,70 @@ const MoodScreen = () => {
                 >
                   {date.day}
                 </Text>
-                {marking === undefined
-                  ? console.log(marking)
-                  : console.log(marking.type)}
-                {marking !== undefined ? (
+                {marking !== undefined && marking.type === "happy" ? (
                   <Image
-                    style={{ width: 50, height: 100 }}
+                    style={{
+                      width: moderateScale(46),
+                      height: moderateScale(90),
+                      marginBottom: moderateScale(-30),
+                    }}
+                    source={require("../assets/image/alien-happy.png")}
+                  />
+                ) : marking !== undefined && marking.type === "sad" ? (
+                  <Image
+                    style={{
+                      width: moderateScale(46),
+                      height: moderateScale(90),
+                      marginBottom: moderateScale(-30),
+                    }}
+                    source={require("../assets/image/alien-sad.png")}
+                  />
+                ) : marking !== undefined && marking.type === "okay" ? (
+                  <Image
+                    style={{
+                      width: 50,
+                      height: moderateScale(95),
+                      marginTop: moderateScale(-10),
+                      marginBottom: moderateScale(-30),
+                    }}
+                    source={require("../assets/image/alien-okay2.png")}
+                  />
+                ) : marking !== undefined && marking.type === "mad" ? (
+                  <Image
+                    style={{
+                      width: moderateScale(46),
+                      height: moderateScale(110),
+                      marginTop: moderateScale(-15),
+                      marginBottom: moderateScale(-30),
+                    }}
+                    source={require("../assets/image/alien-mad.png")}
+                  />
+                ) : marking !== undefined && marking.type === "amazing" ? (
+                  <Image
+                    style={{
+                      width: moderateScale(46),
+                      height: moderateScale(90),
+                      marginBottom: moderateScale(-30),
+                    }}
                     source={require("../assets/image/alien-amazing.png")}
                   />
                 ) : (
                   <View
-                    style={{ width: 50, height: 80, backgroundColor: "gray" }}
+                    style={{
+                      width: moderateScale(46),
+                      height: moderateScale(78),
+                      backgroundColor: "#D3D3D3",
+                      borderRadius: 20,
+                    }}
                   ></View>
                 )}
               </View>
             );
           }}
-          markedDates={{
-            "2022-09-03": { type: "happy" },
-            "2022-09-17": { marked: true },
-            "2022-09-30": { marked: true },
-          }}
+          markedDates={data}
         />
         {editTrue === false ? (
-          <TouchableWithoutFeedback onPress={() => setEditTrue(true)}>
+          <TouchableWithoutFeedback onPress={handleSubmit}>
             <AntDesign
               name="pluscircleo"
               size={moderateScale(24.2)}
@@ -85,7 +153,7 @@ const MoodScreen = () => {
             />
           </TouchableWithoutFeedback>
         ) : (
-          <TouchableWithoutFeedback onPress={() => setEditTrue(false)}>
+          <TouchableWithoutFeedback onPress={handleSubmit}>
             <Feather name="edit-2" size={moderateScale(24.2)} color="black" />
           </TouchableWithoutFeedback>
         )}
@@ -186,6 +254,7 @@ const styles = StyleSheet.create({
   },
   Calendar: {
     width: moderateScale(350),
+    borderRadius: moderateScale(8),
   },
 });
 
